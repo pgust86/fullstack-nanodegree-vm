64e0fc0acf3e070ec44b54b7da79adfad741f7ab
Peter Gust <eagles157@gmail.com>
2018-05-23 22:30:48 -0400

    finished lesson one working with CRUD with Udacity Nano Degree

---
+++ vagrant/project/database_setup.py
@@ -0,0 +1,46 @@
+import sys
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy import create_engine
+
+Base = declarative_base()
+
+class Restaurant(Base):
+    __tablename__='restaurant'
+
+    name = Column(
+    String(80), nullable = False)
+    id = Column(
+    Integer, primary_key = True)
+
+class MenuItem(Base):
+
+    __tablename__='menu_item'
+
+    name = Column(
+    String(80), nullable = False)
+    id = Column(Integer, primary_key = True)
+    course = Column(String(250))
+    description = Column(String(250))
+    price = Column(String(8))
+    restaurant_id = Column(
+    Integer, ForeignKey('restaurant.id'))
+    restaurant = relationship(Restaurant)
+
+########insert at end of file##########
+engine = create_engine(
+'sqlite:///restaurantmenu.db')
+Base.metadata.create_all(engine)
+
+
+
+#code to setup session
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from database_setup import Base, Restaurant, MenuItem
+engine = create_engine('sqlite:///restaurantmenu.db')
+Base.metadata.bind = engine
+DBSession = sessionmaker(bind = engine)
+session = DBSession
